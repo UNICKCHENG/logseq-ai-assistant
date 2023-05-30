b2cdafa8338024b8ff0fee8207b5ef42aa13959a
zhicheng <hi@unickcheng.cc>
2023-05-30 21:49:59 +0800

    feat: support stream output

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -2,22 +2,20 @@ import { getSettings } from './settings';
 
 export const chat = async(content: string) => {
     const baseUrl: string = `${(await getSettings()).baseUrl}/v1/chat/completions`;
-    const response = await fetch(`${baseUrl}`, {
-        method: 'POST',
-        headers: { 
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${(await getSettings()).apiKeys}`
-        },
-        body: JSON.stringify({
-            "model": "gpt-3.5-turbo",
-            "messages": [{"role": "user", "content": content}]
-        })
-    });
-
-    const data = await response.json();
-    if (response.ok) {
-        return data.choices[0].message.content;
-    } else {
-        throw new Error(data.message);
+    try {
+        return await fetch(`${baseUrl}`, {
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${(await getSettings()).apiKeys}`
+            },
+            body: JSON.stringify({
+                "model": "gpt-3.5-turbo",
+                "messages": [{"role": "user", "content": content}],
+                "stream": true,
+            })
+        });
+    } catch (err: any) {
+        throw new Error(err.message);
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,36 @@ logseq.useSettingsSchema(settingsSchema);
 
 function main () {
     logseq.Editor.registerSlashCommand('gpt',
-    async() => {
-        const { content, uuid }: any = await logseq.Editor.getCurrentBlock();
-        logseq.Editor.insertBlock(uuid, await chat(content));
+        async() => {
+            let { content, uuid }: any = await logseq.Editor.getCurrentBlock();
+            uuid = (await logseq.Editor.insertBlock(uuid, `loading...`))?.uuid;
+
+            let result: string = "";
+            const decoder = new TextDecoder("utf-8");
+            const reader = (await chat(content)).body?.getReader();
+            while (true) {
+                const { done, value }: any = await reader?.read();
+                if (done) {
+                    break;
+                }
+                // Massage and parse the chunk of data
+                const chunk = decoder.decode(value);
+                const lines = chunk.split("\n");
+                const parsedLines = lines
+                    .map((line) => line.replace(/^data: /, "").trim()) // Remove the "data: " prefix
+                    .filter((line) => line !== "" && line !== "[DONE]") // Remove empty lines and "[DONE]"
+                    .map((line) => JSON.parse(line)); // Parse the JSON string
+
+                for (const parsedLine of parsedLines) {
+                    const { choices } = parsedLine;
+                    const { delta } = choices[0];
+                    const { content } = delta;
+                    if (content) {
+                        result += content;
+                    }
+                }
+                await logseq.Editor.updateBlock(uuid, result);
+            }
     });
 }
 
